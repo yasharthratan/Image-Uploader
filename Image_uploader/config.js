efb8557eb04f40a449f130cf559f425c5fc267c0
Yasharth Tripathi <yasharthtripathi@gmail.com>
2021-05-17 01:18:06 +0530

    Update config.js

--- Image_uploader/config.js
+++ Image_uploader/config.js
@@ -1,6 +1,8 @@
 
 const config = {
-    MONGO_URI : "mongodb+srv://admin:admin123@cluster0.eaah3.mongodb.net/uploader?retryWrites=true&w=majority"
+    MONGO_URI : "mongodb+srv://admin:@cluster0.eaah3.mongodb.net/uploader?retryWrites=true&w=majority"
 }
 
 module.exports = config;
+
+//password in different file to prevent security breach in the database
